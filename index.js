c84dc29bffcf4c0e3b53a1a6e332acce061ce0c0
joao.oliveira0117@hotmail.com <joao.oliveira0117@hotmail.com>
2020-07-02 22:21:08 -0300

    create remove card function, placed the remove btn

--- index.js
+++ index.js
@@ -33,8 +33,10 @@ function addNewScrap() {
 
 function createScrapCard(title, message, position) {
   return `
-  <div class="message-cards card text-white bg-dark m-2 col-3">
-    <div class="card-header font-weight-bold">${title}</div>
+  <div class="message-card card text-white bg-dark m-2 col-3" id="card${position}">
+    <div class="card-header font-weight-bold">${title}
+        <input type="image" src="/img/icon.png" class="removeButton" onclick="removeScrapCard(${position})">
+    </div>
     <div class="card-body">
       <p class="card-text">
         ${message}
@@ -42,6 +44,11 @@ function createScrapCard(title, message, position) {
     </div>
   </div>
   `;
+}
+
+function removeScrapCard(index){
+    let scrapcard = document.getElementById(`card${index}`);
+    scrapcard.remove();
 }
 
 function saveInStorage(){
