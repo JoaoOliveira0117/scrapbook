c4406e5cb03a3c7ebc38b4777d5473315cb11f50
joao.oliveira0117@hotmail.com <joao.oliveira0117@hotmail.com>
2020-07-16 21:14:18 -0300

    add Scraps functions

--- main.js
+++ main.js
@@ -6,7 +6,46 @@ class TaskList {
         this.scrapsField = document.getElementById("scrapsField");
         
         this.scraps = [];
+
+        this.registerAddScrapBtnEvent();
+    }
+
+    registerAddScrapBtnEvent(){
+        this.addScrapBtn.onclick = () => this.addNewScrap();
+    }
+
+    renderScraps() {
+        this.scrapsField.innerHTML = "";
+
+        for (const scrap of this.scraps) {
+            this.scrapsField.innerHTML += this.createScrapCard(scrap.title,scrap.message);
+        }
+    }
+
+    addNewScrap(){
+        let title = this.titleInput.value;
+        let message = this.messageInput.value;
+
+        this.titleInput.value = "";
+        this.messageInput.value = "";
+
+        this.scraps.push({title,message});
+        this.renderScraps();
+    }
+
+    createScrapCard(title,message){
+        return `
+        <div class="message-cards card text-white bg-dark m-2 col-3">
+            <div class="card-header font-weight-bold" id="titulo">${title}</div>
+            <div class="card-body">
+                <p class="card-text" id="mensagem">${message}</p>
+            </div>
+            <div class="w-100 d-flex justify-content-end pr-2 pb-2">
+                <button class="btn btn-danger mr-1">Deletar</button>
+                <button class="btn btn-info">Editar</button>
+            </div>
+        </div>
+        `;
     }
 }
-
 new TaskList();
